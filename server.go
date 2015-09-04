791bff8c27d5f3221db15390b2086f680f615bb5
shonenada <shonenada@gmail.com>
2015-09-04 22:03:28 +0800

    update

--- server.go
+++ server.go
@@ -1,10 +1,15 @@
 package heron
 
 import (
+    "os"
+    "log"
     "github.com/zenazn/goji"
 )
 
+var logger = log.New(os.Stdout, "[heron]: ", log.LstdFlags)
+
 func StartServer () {
+    Init()
     SetupRoutes()
     goji.Serve()
 }
