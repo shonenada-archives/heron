e088527bca94d441bcfdaebdf9080d821043ae0d
shonenada <shonenada@gmail.com>
2015-09-05 22:15:57 +0800

    insert user record

--- account.go
+++ account.go
@@ -2,16 +2,28 @@ package heron
 
 import (
     "net/http"
+
     "github.com/zenazn/goji/web"
+
+    "github.com/shonenada/heron/models"
 )
 
-func AccountJoinController(c web.C, w http.ResponseWriter, r *http.Request) {
-    switch r.Method {
-    case "GET":
-        RenderTemplate(w, "account_join.html", "base.html", map[string]interface{}{
-        })
-        break
-    case "POST":
-        break
+func AccountController(c web.C, w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    db := GetDatabase()
+    username := r.PostForm.Get("username")
+    password := r.PostForm.Get("password")
+    user := models.Account{Username: username, Password: password}
+    record := db.NewRecord(user)
+    if record {
+        db.Create(&user)
     }
+    RenderJson(w, map[string]interface{}{
+        "success": true,
+        "info": "inserted",
+    })
+}
+
+func AccountJoinController(c web.C, w http.ResponseWriter, r *http.Request) {
+    RenderTemplate(w, "account_join.html", "base.html", nil)
 }
