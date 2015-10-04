153ad859de0bd66baa0b33344d7e9efd7c2c518c
shonenada <shonenada@gmail.com>
2015-10-04 18:23:41 +0800

    add migration

--- db.go
+++ db.go
@@ -1,15 +1,26 @@
 package heron
 
 import (
-    "github.com/jinzhu/gorm"
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"github.com/shonenada/heron/models"
 )
 
+func GetDatabase() gorm.DB {
+	db, err := gorm.Open("mysql", Config.DatabaseURI)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
 
-func GetDatabase() (gorm.DB){
-    db, err := gorm.Open("mysql", Config.DatabaseURI)
-    if err != nil {
-        panic(err)
-    }
-    return db
+func Migrate() {
+	db := GetDatabase()
+	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&models.Account{})
+	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&models.Event{})
+	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&models.Follow{})
+	db.Model(&models.Event{}).AddForeignKey("user_id", "users(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Follow{}).AddForeignKey("user_id", "users(id)", "RESTRICT", "RESTRICT")
+	db.Model(&models.Follow{}).AddForeignKey("follow_id", "users(id)", "RESTRICT", "RESTRICT")
+	db.AutoMigrate(&models.Account{}, &models.Follow{}, &models.Event{})
 }
